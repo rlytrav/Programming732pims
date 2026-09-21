f7755b7f0a6b63539279e7740efc2ec70644554a
rlytrav <ngobenimohau4@gmail.com>
2026-09-21 07:27:36 +0200

    Add database connection class

---
+++ src/main/java/com/healthfirst/pims/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package com.healthfirst.pims;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/healthfirst_pims";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "MySQL password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
